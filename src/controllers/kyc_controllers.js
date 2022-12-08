9eb376c2d171160f7bdd02804dcb61b6f207c930
nishaflyweis <nishaflyweis@gmail.com>
2022-12-08 14:01:36 +0530

    Changes toImage Handler

--- src/controllers/kyc_controllers.js
+++ src/controllers/kyc_controllers.js
@@ -2,47 +2,24 @@ const  multer = require('multer');
 const path = require('path'); 
 
 
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, './documents');
-//     },
-//     filename: function (req,res, file, cb) {
-//       console.log(req.originalname)
-//       if(req.originalname === null){
-//          res.status(400).json("Select your File ")
-//       }
-//         const uniqueName = `${Date.now()}-${Math.round(
-//             Math.random() * 1e9
-//           )}${path.extname(file.originalname)}`;
-//           cb(null, uniqueName);
-//     }
-//   });
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
       cb(null, './documents');
     },
-  filename: function (req, file, cb) {
+    filename: function (req, file, cb) {
       cb(null, file.originalname);
   }
 });
 
+const  upload = multer({ storage : storage }).array('document',3);
 
-exports.uploadImg =  multer({ storage : storage }).array('document',3);
 
-
-exports.uploadKyc = async(req, res) => {
-  try{
-  const data = {
-      image: req.file.path
-  }
-console.log(data)
-res.send("Kyc is Done ");
-}catch(err){
-  console.log(err);
-  res.status(400).json({
-      message: err.message
-  })
-
-}
+exports.uploadsKyc = (req,res) => {
+  console.log("checks");
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.send("Kyc is Done ");
+    });
 }
