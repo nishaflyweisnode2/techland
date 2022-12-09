16b44d59431eb8b2863667c1ed25ec4a4cc3dd0e
nishaflyweis <nishaflyweis@gmail.com>
2022-12-09 13:05:54 +0530

    Changes

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -5,13 +5,18 @@ const AdminModel = require('../models/admin_model');
 module.exports = {
     isAdmin: async (req, res, next) =>{
            const AdminData = await AdminModel.find({email: req.body.email})
-            console.log(AdminData[0].role);
-            if(AdminData[0].role === 'admin'){
-                next()
-            }else {
-                res.status(400).json({
-                    message : "You are not Admin "
-                })
+           console.log(AdminData)
+            if(AdminData.length == 0){
+                res.status(200).json({send: "You are not Admin "})
+            }else{
+                if(AdminData[0].role === 'admin'){
+                    next()
+                }else {
+                    res.status(400).json({
+                        message : "You are not Admin "
+                    })
+                }
             }
+           
         }
     }
